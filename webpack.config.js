00ac534e17f2b2bd3c1e4b976e7422342885a68f
Mauricio Miranda <maurimiranda@gmail.com>
2016-10-31 10:29:24 -0300

    Add fontawesome as dependency. Refactor sass imports.

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 /* eslint-disable */
 
 const webpack = require('webpack');
-const yargs = require('yargs');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const env = yargs.argv.mode;
 const libraryName = 'GeoDashboard';
 const outputFile = 'geo-dashboard.js';
 const plugins = [
-  new ExtractTextPlugin('geo-dashboard.css')
+  new ExtractTextPlugin('geo-dashboard.css'),
+  new webpack.optimize.UglifyJsPlugin({
+    minimize: true,
+    compress: {
+      warnings: false
+    }
+  }),
 ];
-
-if (env === 'build') {
-  plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }));
-}
 
 module.exports = {
   entry: path.join(__dirname, '/src/scripts/main.js'),
@@ -27,25 +27,31 @@ module.exports = {
   },
   devtool: 'source-map',
   module: {
+    noParse: [path.join(__dirname, 'node_modules/openlayers/dist/ol.js')],
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel',
-        exclude: /node_modules/
+        loader: 'babel-loader',
+        exclude: /node_modules/,
       },
       {
         test: /\.js$/,
-        loader: "eslint-loader",
+        loader: 'eslint-loader',
         exclude: /node_modules/
       },
       {
         test: /\.hbs$/,
-        loader: "handlebars-loader"
+        loader: 'handlebars-loader'
       },
       {
         test: /\.scss$/,
         loader: ExtractTextPlugin.extract(['css?sourceMap', 'sass?sourceMap'])
-      }
+      },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=image/svg+xml' },
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/font-woff" },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
     ],
   },
   plugins,
